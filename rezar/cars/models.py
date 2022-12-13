e4dee9e5a489b0d0627f784a5b3f2a119e5fc176
KyryloSer <kirser.h@gmail.com>
2022-12-13 19:51:59 +0200

    models car and news

--- rezar/cars/models.py
+++ rezar/cars/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
 # Create your models here.
+class Brand(models.Model):
+    brand_name = models.CharField(max_length=50, blank=False)
+
+
+class CarShape(models.Model):  # тип кузова
+    shape = models.CharField(max_length=20, blank=True)
+
+
+class Engine(models.Model):
+    size = models.CharField(max_length=50, blank=True)
+    power_reserve = models.IntegerField(blank=True)
+    speed = models.IntegerField(blank=True)
+
+
+class CarModel(models.Model):
+    model_name = models.CharField(max_length=50, blank=False)
+    trim = models.CharField(max_length=50, blank=True)  # комплектация
+    car_shape = models.ForeignKey(CarShape, to_field='id', on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, to_field='id', on_delete=models.CASCADE)
+    engine = models.ForeignKey(Engine, to_field='id', on_delete=models.CASCADE)
+
+
+class Car(models.Model):
+    price = models.IntegerField(blank=True)
+    car_model = models.ForeignKey(CarModel, to_field='id', on_delete=models.CASCADE)
--- rezar/news/models.py
+++ rezar/news/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=150, blank=False)
+    content = models.TextField(blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='news/%Y/%m/%d/', blank=True)
+    is_published = models.BooleanField(default=True)
